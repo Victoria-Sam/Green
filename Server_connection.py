0d58bbcf4c6593105a482c16c1c3edc90c21fa49
Pavel Kazantsev <kazantsev.pavel.s@gmail.com>
2019-11-16 16:05:10 +0300

    Added method to convert json representation into graph

--- Server_connection.py
+++ Server_connection.py
@@ -1,9 +1,27 @@
 import socket
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
 sock = socket.socket()
 sock.connect(('wgforge-srv.wargaming.net', 443))
 actions = {'LOGIN': 1, 'LOGOUT': 2, 'MOVE': 3, 'UPGRADE': 4, 'TURN': 5,
            'PLAYER': 6, 'GAMES': 7, 'MAP': 10}
+
+
+class JsonParser:
+    @staticmethod
+    def json_to_graph(result):
+        if result["result_code"] == 0:
+            nodes = result['response']['points']
+            edges = result['response']['lines']
+            g = nx.Graph()
+            g.add_nodes_from([x['idx'] for x in nodes])
+            g.add_edges_from(
+                [x['points']+[{'length': x['length']}] for x in edges]
+            )
+            return g
+        else:
+            return None
 
 
 def message_to_server(action, **kwargs):
@@ -30,42 +48,41 @@ def message_to_server(action, **kwargs):
 
 
 result = message_to_server('LOGIN', name="Boris")
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 result = message_to_server('MAP', layer=0)
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
+map_graph = JsonParser.json_to_graph(result)
+nx.draw(map_graph)
+plt.show()
 result = message_to_server('MAP', layer=1)
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 result = message_to_server('MAP', layer=10)
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
-result = message_to_server('MAP', layer=10)
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 result = message_to_server('GAMES')
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 result = message_to_server('TURN')
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 result = message_to_server('UPGRADE', posts=[], trains=[1])
-print("Result code:", result["result_code"])
-print("Response length:", result["response_length"])
-print("Response:")
-print(result["response"])
+# print("Result code:", result["result_code"])
+# print("Response length:", result["response_length"])
+# print("Response:")
+# print(result["response"])
 sock.close()
